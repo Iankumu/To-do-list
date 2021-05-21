968a0799b7cfaa6c967b0f5e789cb5581766edc9
iankumu <ikariuki741>
2021-05-21 08:04:54 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form'
+import TodoList from './components/TodoList'
+import {useState, useEffect} from 'react'
 
 function App() {
+  //States
+  const [inputText,setInputText] = useState("");
+  const [todos,setTodos] = useState([]);
+  const [status,setStatus]=useState("all")
+  const [filteredTodos,setFilteredTodos] = useState([])
+
+
+  useEffect(()=>{
+    getLocalTodos()
+  },[])
+
+  useEffect(()=>{
+    filterHandler()
+  },[status,todos])
+
+
+  // Functions
+
+  const filterHandler = () => {
+    switch(status){
+    case 'completed':
+      setFilteredTodos(todos.filter(todo => todo.completed ===true))
+      break
+    case 'uncompleted':
+      setFilteredTodos(todos.filter(todo => todo.completed ===false))
+      break
+    default:
+      setFilteredTodos(todos)    
+    }
+  }
+  // Save to Local todos
+
+  const saveToLocalTodos = () => {
+    localStorage.setItem('todos',JSON.stringify(todos))
+  }
+
+  const getLocalTodos = () =>{
+    if(localStorage.getItem('todos') === null)
+    {
+      localStorage.setItem('todos',JSON.stringify([]))
+    }else{
+      let todoLocal= JSON.parse(localStorage.getItem("todos"))
+      setTodos(todoLocal)
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <header><h1>To-do List</h1></header> 
+     <Form setInputText={setInputText}  todos={todos} setStatus={setStatus} setTodos={setTodos} inputText={inputText} />
+     <TodoList filteredTodos={filteredTodos} setTodos={setTodos} todos={todos}/>
     </div>
+   
   );
 }
 
